56963b3c8e2d304349d0ebb3b5732ae78bc5743d
sbetta <90723625+bettyspaghetty@users.noreply.github.com>
2021-09-28 22:13:38 +0200

    updateSpaghetty

--- sketch.js
+++ sketch.js
@@ -1,13 +1,42 @@
-function preload() {
-  // put preload code here
-}
-
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  // put setup code here
+  createCanvas(400, 400, WEBGL);
 }
 
 function draw() {
-  // put drawing code here
-  background("red");
+  background(0);
+
+  noFill();
+  stroke(1);
+
+  rotateX(frameCount / 100);
+  rotateY(frameCount);
+
+  // Hold down to rotateZ
+  if (mouseIsPressed) {
+    rotateZ(frameCount / 200);
+  } else {
+    rotateZ(0);
+  }
+
+  // loop to generate the sphere
+  for (var i = 0; i < map(mouseX, 20, 200, 0, width); i++) {
+    // to prevent the loops to affect each other
+    push();
+
+    // colors
+    var r = map(sin(i + frameCount), -1, 1, 255, 255);
+    var g = map(sin(i * frameCount), -1, 1, 0, 204);
+    var b = map(cos(i), -1, 1, 204, 0);
+
+    stroke(r, g, b);
+
+    // rotation Y 180°
+    rotateY(i / 2);
+    rotateZ(i / 7);
+
+    // circle centered in (0, 0)
+    ellipse(0, 0, 200);
+
+    pop();
+  }
 }
